2bedb236a3ff0ef6451ce0aac69e57795887d843
Maxim Learner <maxim.learner@yandex.ru>
2023-01-03 20:54:06 +0600

    Java tasks from QA Automation training ready for code review

---
+++ airport/src/main/java/Planes/experimentalPlane.java
@@ -0,0 +1,41 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalPlaneTypes;
+
+public class experimentalPlane extends Plane{
+
+    private ExperimentalPlaneTypes planeType;
+    private ClassificationLevel planeClassificationLevel;
+
+    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalPlaneTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.planeType = type;
+        this.planeClassificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel(){
+        return planeClassificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel){
+        this.planeClassificationLevel = classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "experimentalPlane{" +
+                "model = '" + getModel() + '\'' +
+                '}';
+    }
+}
---
+++ airport/src/main/java/models/MilitaryPlainTypes.java
@@ -0,0 +1,5 @@
+package models;
+
+public enum MilitaryPlainTypes {
+    FIGHTER, BOMBER, TRANSPORT
+}
